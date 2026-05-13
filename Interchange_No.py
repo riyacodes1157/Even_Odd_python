feebf9b8e72307bc9669cb87fe0aec169b0231fd
riyacodes1157 <riyasonawane2007@gmail.com>
2026-05-13 11:04:13 +0530

    Update Interchange_No.py

--- Interchange_No.py
+++ Interchange_No.py
@@ -1,1 +1,7 @@
-
+n1=int(input("Enter the first number:"))
+n2=int(input("Entr the second number:"))
+print("Before interchang %d and %d",n1,n2)
+temp=n1
+n1=n2
+n2=temp
+print("After interchang %d and %d",n1,n
